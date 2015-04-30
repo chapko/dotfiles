f83278496dd56ea17ab989e1d667c636f52ee9f4
Eugene Chapko <eugene.chapko@gmail.com>
2015-04-30 15:49:29 +0300

    [git] move git config to the git folder. some tweaks and fixes

---
+++ git/completion.zsh
@@ -0,0 +1,8 @@
+compdef _git gits=git-status
+compdef _git gitd=git-diff
+compdef _git gitdl=git-diff
+compdef _git gitdc=git-diff
+compdef _git gita=git-add
+
+compdef gitp=git
+compdef qgit=git
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,12 +5,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' insert-tab pending
 
 compdef sc=systemctl
-
-compdef _git gits=git-status
-compdef _git gitd=git-diff
-compdef _git gitdl=git-diff
-compdef _git gitdc=git-diff
-compdef _git gita=git-add
-
-compdef gitp=git
-compdef qgit=git

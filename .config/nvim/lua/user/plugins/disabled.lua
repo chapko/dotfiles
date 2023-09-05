72448e460d4d9a375f46ee02c1209ed79ad2cc31
Eugene Chapko <eugene.chapko@gmail.com>
2023-09-05 18:58:43 +0400

    nvim: refactor config

---
+++ .config/nvim/lua/user/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  -- breaks macros replay
+  { "max397574/better-escape.nvim", enabled = false },
+
+  -- not used
+  { "stevearc/aerial.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "mfussenegger/nvim-dap", enabled = false },
+}
---
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -0,0 +1,43 @@
+local ignore_patterns = {
+  "node_modules/",
+  "%.git/branches/",
+  "%.git/objects/",
+  "%.git/lfs/",
+  "%.git/logs/",
+  "%.git/refs/",
+  "%.git/rr%-cache/",
+  "%.git/index$",
+}
+
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      local actions = require "telescope.actions"
+
+      return vim.tbl_deep_extend("force", opts, {
+        defaults = {
+          preview = {
+            treesitter = false,
+          },
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.cycle_history_next,
+              ["<C-k>"] = actions.cycle_history_prev,
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            file_ignore_patterns = ignore_patterns,
+          },
+          live_grep = {
+            file_ignore_patterns = ignore_patterns,
+          },
+        },
+      })
+    end,
+  },
+}

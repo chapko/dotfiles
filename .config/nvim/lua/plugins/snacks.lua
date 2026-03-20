093bff31a30b44acbbbe6cd201be34f38e882e0e
Eugene Chapko <eugene.chapko@gmail.com>
2026-03-20 17:43:55 +0400

    nvim dashboard config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,5 +6,16 @@ return {
   ---@type snacks.Config
   opts = {
     scroll = { enabled = false },
+    dashboard = {
+      preset = {
+        header = "🍉",
+      },
+      sections = {
+        { section = "header" },
+        { title = "Recent", section = "recent_files", padding = 1 },
+        { title = "Projects", section = "projects", padding = 1 },
+        { section = "startup" },
+      },
+    },
   },
 }
